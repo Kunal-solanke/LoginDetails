ad463f7088e68ae9172509a070b67dc0c2e2f66a
Kunal-solanke <kunalsolanke1993@gmail.com>
2022-12-29 16:02:32 +0530

    first commit

---
+++ src/main/java/com/LoginDetails/Entity/LoginDetails.java
@@ -0,0 +1,5 @@
+package com.LoginDetails.Entity;
+
+public class LoginDetails {
+
+}
---
+++ src/main/java/com/LoginDetails/LoginDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.LoginDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginDetailsApplication.class, args);
+	}
+
+}
